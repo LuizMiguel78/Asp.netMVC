8766423a93530adcb690af0a5aaca4ae51fcb6ff
LuizMiguel78 <149170949+LuizMiguel78@users.noreply.github.com>
2026-04-27 16:41:46 -0300

    estado

---
+++ Miguel/MiguelMVC/MiguelMVC/Controllers/EstadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiguelMVC.Controllers
+{
+    public class EstadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
