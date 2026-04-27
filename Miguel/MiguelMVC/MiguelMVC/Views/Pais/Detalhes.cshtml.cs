afbe7fbb6e5a138a1884157a58b6faaebf039780
LuizMiguel78 <149170949+LuizMiguel78@users.noreply.github.com>
2026-04-27 16:33:44 -0300

    criar país

---
+++ Miguel/MiguelMVC/MiguelMVC/Views/Pais/Detalhes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiguelMVC.Views.Pais
+{
+    public class DetalhesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
